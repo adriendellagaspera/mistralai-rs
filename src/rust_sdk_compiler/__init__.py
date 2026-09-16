1bed0ed0b05eeeb0ca9d9b937d62c729cb7234aa
adriendellagaspera <32226398+adriendellagaspera@users.noreply.github.com>
2026-09-16 14:25:03 +0200

    chore: expose compiler patch version

--- src/rust_sdk_compiler/__init__.py
+++ src/rust_sdk_compiler/__init__.py
@@ -3,7 +3,7 @@
 from .api import Bindings, Compilation, OpenApi, Policy, Runtime, compile, lower
 from .sdk_frontend import GenerationError
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "Bindings",
